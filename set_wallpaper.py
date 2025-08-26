ef60cfbc86d72620db08f24f31335438bf2d7fbc
Smee18 <marcel.venturotti@icloud.com>
2025-08-26 23:12:05 +0300

    Comments

--- set_wallpaper.py
+++ set_wallpaper.py
@@ -7,13 +7,21 @@ from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
 from datetime import datetime, timedelta
 
-
+##Remove mars pics folder to avoid having to store tons
 shutil.rmtree('images/mars_pics', ignore_errors=True)
-API_KEY = {} ##SET YOUR API HERE AS A STRING
+
+API_KEY = "rdUInhYvw4M9497vkPj6AO1xYTa0DGOCgy1ph4xN"
 
 manifest_url = f"https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key={API_KEY}"
+
 manifest_response = requests.get(manifest_url)
+if manifest_response.status_code != 200:
+    raise Exception(f"Failed to fetch Rover info: {manifest_response.status_code}")
+
 manifest_data = manifest_response.json()['photo_manifest']
+
+print(manifest_data, "\n")
+
 max_sol = manifest_data['max_sol']
 landing_date_str = manifest_data['landing_date']
 landing_date = datetime.strptime(landing_date_str, "%Y-%m-%d")
@@ -21,6 +29,7 @@ print(f"Latest sol with photos: {max_sol}")
 
 url_APOD = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
 url_MARS = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={max_sol}&api_key={API_KEY}"
+
 
 response_APOD = requests.get(url_APOD)
 if response_APOD.status_code != 200:
@@ -32,9 +41,9 @@ if response_MARS.status_code != 200:
 
 data_APOD = response_APOD.json()
 data_MARS = response_MARS.json()
+manifest_data = manifest_response.json()['photo_manifest']
 
 print(data_APOD, '\n')
-
 print(data_MARS)
 
 # Only proceed if it's an image
@@ -50,24 +59,28 @@ mars_photos = data_MARS['photos']
 os.makedirs('images', exist_ok=True)
 os.makedirs('images/mars_pics', exist_ok=True)
 
+# Extract rover pics
 for i, photo in enumerate(mars_photos, start=1):
     img_url = photo['img_src']
     camera = photo['camera']['name']
-    img_data = requests.get(img_url).content
-    file_path = f'images/mars_pics/sol_{max_sol}-{camera}_{i}.jpg'
-    with open(file_path, 'wb') as file:
-        file.write(img_data)
-    print(f"Saved {file_path}")
+    img_data_mars = requests.get(img_url)
+    if img_data_mars.status_code == 200:
+        file_path = f'images/mars_pics/sol_{max_sol}-{camera}_{i}.jpg'
+        with open(file_path, 'wb') as file:
+            file.write(img_data_mars.content)
+        print(f"Saved {file_path}")
+    else:
+        raise Exception(f"Failed to download image: {img_data_mars.status_code}")
 
-
-img_response = requests.get(image_url)
-if img_response.status_code == 200:
-    image_path = f'images/{image_date}.jpg'
-    with open(image_path, 'wb') as file:
-        file.write(img_response.content)
-    print(f"Saved: {image_path}")
+# Extract APOD
+img_data_APOD = requests.get(image_url)
+if img_data_APOD.status_code == 200:
+    APOD_image_path = f'images/{image_date}.jpg'
+    with open(APOD_image_path, 'wb') as file:
+        file.write(img_data_APOD.content)
+    print(f"Saved: {APOD_image_path}")
 else:
-    raise Exception(f"Failed to download image: {img_response.status_code}")
+    raise Exception(f"Failed to download image: {img_data_APOD.status_code}")
 
 # --- CONFIG ---
 wallpaper_size = (1920, 1080)
@@ -75,12 +88,13 @@ left_width = wallpaper_size[0] // 2 + 50
 right_width = wallpaper_size[0] - left_width
 background_color = (0, 0, 0)
 
-apod_path = image_path
+apod_path = APOD_image_path
 rover_paths = []
 
 directory = Path("images/mars_pics")
 mars_files = list(directory.glob("*.jpg")) + list(directory.glob("*.jpeg"))
-for img in mars_files:
+
+for img in mars_files: # Obtain at most 4 pics from NAV cams. Not best code as doesn't format nicely if not enough pics
     if len(rover_paths) == 4:
         break
     elif 'NAV' in img.name:
@@ -99,7 +113,7 @@ apod_x = (left_width - apod_img.width) // 2
 apod_y = 50
 wallpaper.paste(apod_img, (apod_x, apod_y))
 
-# --- RIGHT HALF: ROVER PICS (1x3 grid) ---
+# --- RIGHT HALF: ROVER PICS (2x2 grid) ---
 cols, rows = 2, 2
 img_w = round(right_width / (cols + 0.6))
 img_h = round(wallpaper_size[1] / (rows + 0.6))
@@ -111,6 +125,7 @@ for idx, rover_path in enumerate(rover_paths[:4]):
     y = (idx // cols) * img_h + (img_h - img.height) // 2
     wallpaper.paste(img, (x + ((30 * idx) %60), y + 20))
 
+# --- Text and conversion ---
 sol_length_hours = 24 + 39/60 + 35/3600
 
 now = datetime.now()
@@ -118,7 +133,6 @@ elapsed_hours = (now - landing_date).total_seconds() / 3600
 current_sol = int(elapsed_hours / sol_length_hours)
 days_since_landing = (now - landing_date).days
 
-# Text
 d = ImageDraw.Draw(wallpaper)
 myFont = ImageFont.load_default(size=30)
 d.text((1200, 860), f"Pictures taken on sol {max_sol}", fill=(255, 255, 255, 255), font=myFont)
